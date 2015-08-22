5e626cb49e8b9a4cda1a31cbc227d44ed64d8d17
Andre Garcia <andre.garcia85@gmail.com>
2015-08-22 18:15:01 -0300

    Changed default twitter result lang. Created about page

--- MyTwitterSearch.py
+++ MyTwitterSearch.py
@@ -3,11 +3,14 @@
 from twitter import *
 
 
+
+config = {}
+execfile("config.py", config)
+
+
 class MyTwitterSearch():
 
 	def __init__(self):
-		config = {}
-		execfile("config.py", config)
 
 		self.twitter = Twitter(
 		        auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
@@ -15,11 +18,11 @@ class MyTwitterSearch():
 
 	def search(self, q, count):
 		search_results = []
-		query = self.twitter.search.tweets(q = q, count = count, lang = 'pt', result_type = 'popular')
+		query = self.twitter.search.tweets(q = q, count = count, lang = 'en', result_type = 'mixed')
 	
 		for result in query["statuses"]:
 			search_results.append(result)
-			#print "(%s) @%s %s" % (result["created_at"], result["user"]["screen_name"], result["text"])
+		
 		return search_results
 
 
@@ -29,6 +32,6 @@ class MyTwitterSearch():
 if __name__=='__main__':
 	ts = MyTwitterSearch()
 	cont = 1
-	for x in ts.search('obama', 10):
+	for x in ts.search('dilma', 10):
 		print str(cont) + ')', x['text'], '\n'
 		cont += 1
